e77f0d7e58fb6bf0f0da7edfd49a46cdcbca285d
Thoobi <sydneydaniel76@gmail.com>
2025-03-14 01:32:38 +0100

    feat: responsiveness of the community page

--- src/components/shared/Community/Header.tsx
+++ src/components/shared/Community/Header.tsx
@@ -9,9 +9,9 @@ const Header = () => {
       }}
       className="font-roboto text-white flex flex-col items-start justify-center gap-8 max-lg:w-full max-lg:px-5"
     >
-      <div className="px-16 flex flex-col items-start justify-center gap-8 max-lg:w-full max-lg:px-5">
+      <div className="px-16 flex flex-col items-start justify-center gap-8 max-lg:w-full max-lg:px-5 max-lg:gap-4">
         <div className="w-[560px] max-lg:w-full">
-          <h1 className="text-[3.5rem]/[72px] font-bold max-lg:text-3xl">
+          <h1 className="text-[3.5rem]/[72px] font-bold max-lg:text-4xl">
             Connect With Fellow Learners in Your Community
           </h1>
         </div>
@@ -19,7 +19,7 @@ const Header = () => {
           Get peer support and build your professional network.
         </p>
         <div className="flex gap-4 max-lg:flex-col">
-          <button className="bg-buttonprimary text-white rounded-[4px] h-[51px] w-[194px] max-lg:h-12 text-lg font-medium">
+          <button className="bg-buttonprimary text-white rounded-[4px] h-[51px] w-[194px] max-lg:h-12 text-lg font-medium max-lg:w-[170px]">
             Join Us Today
           </button>
         </div>
