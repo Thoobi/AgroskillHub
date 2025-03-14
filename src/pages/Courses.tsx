6b1d60f9023a67f736594215757cc5ba27ab9df4
Thoobi <sydneydaniel76@gmail.com>
2025-03-14 01:16:34 +0100

    feat: fix up

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -3,14 +3,14 @@ const Courses = () => {
   return (
     <section className="max-lg:px-5 h-full w-full bg-primarywhite px-16 py-20 flex justify-center items-center font-jarkata">
       <div className="flex justify-center items-center max-lg:w-full flex-col">
-        <div className="w-1/2 mt-20 max-lg:w-full max-lg:px-2">
+        <div className="w-1/2 mt-20 max-lg:w-full">
           <p className="text-5xl font-bold text-center max-lg:text-3xl max-lg:font-semibold ">
             We are still working on this page. Please check back for updates.
           </p>
         </div>
 
-        <div className="max-lg:w-[500px]">
-          <img src={wip} alt="Work in Progress" className="w-full h-full" />
+        <div className="max-lg:w-full">
+          <img src={wip} alt="Work in Progress" className=" h-full" />
         </div>
       </div>
     </section>
