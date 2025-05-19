69948f265f6ec72c6b6f7972c7209c2ad21a73e4
Thoobi <sydneydaniel76@gmail.com>
2025-05-19 16:22:01 +0100

    fix!: fix the auth screen layout

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -2,19 +2,16 @@ import { Outlet, Link } from "react-router-dom";
 
 export default function AuthLayout() {
   return (
-    <section className="bg-[#f2f2e6] flex max-lg:flex-col height flex-row items-center h-screen justify-between max-lg:justify-center font-jarkata w-full p-5 max-lg:py-5">
-      <div
-        className="flex h-full max-lg:h-[400px] w-1/2 max-lg:w-full rounded-lg bg-cover bg-no-repeat bg-center max-md:hidden"
-        style={{
-          backgroundImage: "url('/assets/bg-illus5.jpeg')",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "contain",
-          height: "100%",
-        }}
-      >
-        <span className="flex flex-col items-start justify-start p-5 w-full h-full bg-black/10 text-white rounded-2xl max-md:hidden"></span>
+    <section className="bg-[#f2f2e6] min-h-screen flex max-lg:flex-col flex-row items-stretch justify-between max-lg:justify-center font-jarkata w-full p-5 max-lg:py-5">
+      <div className="flex w-1/2 rounded-lg max-md:hidden">
+        <img
+          src="/assets/bg-illus5.jpeg"
+          alt="background"
+          className="h-full w-full object-cover rounded-lg"
+        />
       </div>
-      <div className="w-1/2 h-full justify-center items-center flex flex-col gap-10 rounded-lg p-5 max-lg:w-full max-lg:p-0 max-lg:py-5 max-lg:gap-5">
+
+      <div className="flex-1 justify-center items-center flex flex-col gap-10 rounded-lg p-5 max-lg:w-full max-lg:p-0 max-lg:py-5 max-lg:gap-5">
         <Link to={"/"} className="h-20 w-20 max-lg:h-10 max-lg:w-10">
           <img src={"/MASTASKILLZ.svg"} alt="the icon logo for the project" />
         </Link>
